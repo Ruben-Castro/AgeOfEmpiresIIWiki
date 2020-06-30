8f0bd2b035abffe14d88f2830570d55043a8f671
Ruben Castro Espinoza <rubencastroe@gmail.com>
2020-06-30 13:14:23 -0700

    Finished Civilizations Detail View

--- AgeOfEmpiresIIWiki/Views/CivilizationDetailsView.swift
+++ AgeOfEmpiresIIWiki/Views/CivilizationDetailsView.swift
@@ -11,23 +11,72 @@ import SwiftUI
 struct CivilizationDetailsView: View {
     var civilizationDetails : Civilization
     var body: some View {
-        VStack {
-            Text(civilizationDetails.name)
-                .font(.title)
-            Text(civilizationDetails.expansion)
-            Text(civilizationDetails.army_type)
-            Text(civilizationDetails.team_bonus)
-        }
+        VStack(alignment: .leading) {
+            Image("Logo")
+                .resizable()
+                .frame(height: 200)
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.bottom)
+
+            HStack {
+                Spacer()
+                Text(civilizationDetails.name)
+                    .font(.system(size: 40, weight: .bold, design: .monospaced))
+                Spacer()
+            }
+            .padding(.bottom)
+            
+           
+            VStack(alignment: .leading) {
+                ExtractedView(title: "Expansion:    ", detail: civilizationDetails.expansion)
+                ExtractedView(title: "Army Type:   ", detail: civilizationDetails.army_type)
+                ExtractedView(title: "Team Bonus:", detail:civilizationDetails.team_bonus)
+            }
+            
+
+            CivBonusView(title: "Civilizations Bonuses", bonuses: civilizationDetails.civilization_bonus)
+            
+        }.background(Color.yellow)
+        
         
     }
 }
 
-
+#if DEBUG
 struct CivilizationDetailsView_Previews: PreviewProvider {
-    static let civ = Civilization(name:"Aztecs", expansion:"sdfasd", army_type:"sdfasdf", unique_unit:["sdfasd","sdfasdf"],
-    unique_tech:["sdafas"], team_bonus:"sdfasdf", civilization_bonus:["sdfasd"])
+    static let civ = Civilization(name:"Aztecs", expansion:"sdfasd", army_type:"sdfasdf",team_bonus:"sdfasdfasfasdfasdfa", civilization_bonus:["sdfasd"])
     static var previews: some View {
         CivilizationDetailsView(civilizationDetails: civ)
     }
 }
+#endif
 
+
+struct ExtractedView: View {
+    var title:String
+    var detail:String
+    var body: some View {
+        HStack {
+            Text(title)
+            .font(.headline)
+            Text(detail)
+          
+        }
+    }
+}
+
+struct CivBonusView: View {
+    var title: String
+    var bonuses: [String]
+    
+    var body: some View {
+            List {
+                Section(header: Text("Civilization Bonuses")){
+                    ForEach(0..<self.bonuses.count){
+                        Text(self.bonuses[$0])
+                        
+                    }
+                }
+            }
+    }
+}
