9f621e996b46ba76ea1e0f75fd4301886effd7bf
Ruben Castro Espinoza <rubencastroe@gmail.com>
2020-06-30 16:06:05 -0700

    finished Tech Detail View

--- AgeOfEmpiresIIWiki/Views/DetailViews/TechDetailsView.swift
+++ AgeOfEmpiresIIWiki/Views/DetailViews/TechDetailsView.swift
@@ -11,14 +11,33 @@ import SwiftUI
 struct TechDetailsView: View {
     var techData: Tech
     var body: some View {
-        Text(techData.name)
+        VStack {
+            Image("units")
+                .resizable()
+                .frame(height:200)
+                .scaledToFill()
+            
+            Text(techData.name)
+            .font(.system(size: 40, weight: .bold, design: .monospaced))
+            
+            VStack(alignment: .leading) {
+                DetailView(title: "Age", detail: techData.age)
+                CostDetailView(resourceType: "Wood", cost: techData.cost.Wood)
+                CostDetailView(resourceType: "Food", cost: techData.cost.Food)
+                CostDetailView(resourceType: "Stone", cost: techData.cost.Stone)
+                CostDetailView(resourceType: "Gold", cost: techData.cost.Gold)
+                DescriptionView(title: "Description", description:techData.description)
+            }
+            .padding(.leading)
+            .padding(.top)
+            
+            Spacer()
+        }.background(Color.yellow)
+       
     }
 }
 
-/*
-struct TechDetailsView_Previews: PreviewProvider {
-    static var previews: some View {
-        TechDetailsView()
-    }
-}
-*/
+
+
+
+
