ac5b0c5fdde4a2a18bd16105cb8420412713e5b4
Ruben Castro Espinoza <rubencastroe@gmail.com>
2020-06-30 15:36:02 -0700

    created tech view

---
+++ AgeOfEmpiresIIWiki/Views/TechView.swift
@@ -0,0 +1,29 @@
+//
+//  TechView.swift
+//  AgeOfEmpiresIIWiki
+//
+//  Created by Ruben Castro Espinoza on 6/30/20.
+//  Copyright © 2020 Ruben Castro Espinoza. All rights reserved.
+//
+
+import SwiftUI
+
+struct TechView: View {
+    var techData: TechnologiesData
+    var body: some View {
+        List(techData.technologies) { tech in
+            NavigationLink(destination: TechDetailsView(techData: tech)){
+                Text(tech.name)
+            }
+        }.navigationBarTitle("Civilizations",displayMode: .inline )
+      
+    }
+}
+
+/*
+struct TechView_Previews: PreviewProvider {
+    static var previews: some View {
+        TechView()
+    }
+}
+*/
