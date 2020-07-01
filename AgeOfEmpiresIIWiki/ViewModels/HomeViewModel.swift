9d902fcb91b04b06a7fce6ea41bbc95b300d74ac
Ruben Castro Espinoza <rubencastroe@gmail.com>
2020-06-30 17:06:33 -0700

    View Model Complete

--- AgeOfEmpiresIIWiki/ViewModels/HomeViewModel.swift
+++ AgeOfEmpiresIIWiki/ViewModels/HomeViewModel.swift
@@ -16,11 +16,12 @@ class HomeViewModel: ObservableObject {
     
     @Published var civilizations  = CivilizationsData(civilizations: [Civilization]())
     @Published var units = UnitsData(units: [Unit]())
-    @Published var tech = []
+    @Published var tech =  TechnologiesData(technologies: [Tech]())
     
     init() {
         fetchCivilizations()
         fetchUnits()
+        fetchTechnologies()
     }
     
     private func fetchCivilizations() {
@@ -68,6 +69,25 @@ class HomeViewModel: ObservableObject {
     }
     
     private func fetchTechnologies() {
+        guard let url = URL(string: apiURL + "technologies") else {return}
+        
+        URLSession.shared.dataTask(with: url) {(data,res,err) in
+            if err != nil {
+                print(err!)
+            }
+            if res != nil {
+                print(res!)
+            }
+            guard let safeData = data else {return}
+            
+            print((String(data: safeData, encoding: String.Encoding.utf8)!))
+            
+            DispatchQueue.main.async {
+                self.tech = try! JSONDecoder().decode(TechnologiesData.self, from: safeData)
+            }
+        }.resume()
+        print("left fetch units")
+        
         
     }
     
